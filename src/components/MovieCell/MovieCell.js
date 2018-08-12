4e4a5769a8ade38a7cd386f6c72803c1ca770807
Koen Romers <info@koenromers.com>
2018-08-12 14:31:15 +0100

    add poster

--- src/components/MovieCell/MovieCell.js
+++ src/components/MovieCell/MovieCell.js
@@ -11,7 +11,12 @@ type Props = {|
 
 function MovieCell(props: Props): Node {
   const { movie } = props;
-  return <div className={styles.item}>{movie.title}</div>;
+  return (
+    <div className={styles.item}>
+      {movie.title}
+      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" />
+    </div>
+  );
 }
 
 export default MovieCell;
